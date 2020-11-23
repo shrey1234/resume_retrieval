e4b9e8342b7dd616f0dc78ebc9c655004a047fa0
shreya goyal <shrey1234@users.noreply.github.com>
2020-11-23 14:41:42 +0530

    Update sqlite.py
    saving cluster into db

--- api/sqlite.py
+++ api/sqlite.py
@@ -1,21 +1,23 @@
 import sqlite3
 import json
+import numpy as np
 
 def create_table(conn):
   conn.execute('DROP TABLE resumes;')
-  conn.execute('CREATE TABLE IF NOT EXISTS resumes (id integer PRIMARY KEY, resume_name text NOT NULL, url text NOT NULL, vector_model json, cluster text );')
+  conn.execute('CREATE TABLE IF NOT EXISTS resumes (id integer PRIMARY KEY, cluster integer, resume_name text NOT NULL, url text NOT NULL, vector_model json );')
   return True
 
-def save_in_db(result, corpus_name):
+def save_in_db(result, corpus_name,cluster):
+  sqlite3.register_adapter(np.int32, int)
   conn = sqlite3.connect('ResumeRetrieval.db')
   create_table(conn)
   values = []
   i = 1
   for value in result:
-    values.append((i, corpus_name[i-1], "resumes/"+corpus_name[i-1], json.dumps(value)))
+    values.append((i,cluster[i-1],corpus_name[i-1], "resumes/"+corpus_name[i-1], json.dumps(value)))
     i += 1
   print("Executing query")
-  result = conn.executemany('INSERT INTO resumes (id, resume_name, url, vector_model) VALUES (?,?,?,?)', values)
+  result = conn.executemany('INSERT INTO resumes (id,cluster,resume_name, url,vector_model) VALUES (?,?,?,?,?)', values)
   conn.commit()
   conn.close()
   return "Successfully inserted"
